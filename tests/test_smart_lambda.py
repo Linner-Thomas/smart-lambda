ddb34273fa9d5104bbfe368aa2d3b08c1ccf4388
Linner Thomas <linner.thomas@gmx.net>
2022-08-10 12:48:04 +0200

    Added additional basic test-cases (no constants, none, str)
    Issue: #9

--- tests/test_smart_lambda.py
+++ tests/test_smart_lambda.py
@@ -32,11 +32,38 @@ class TestSmartLambda(unittest.TestCase):
 
         self.assertEqual(val, return_value, f"Smart-Lambda return-value not matching: {val} != {return_value}")
 
-    @test("SMART-LAMBDA PARSE CONSTANTS-INT")
-    def testParseConstantsInt(self):
+    @test("SMART-LAMBDA PARSE NO-CONSTANT")
+    def testParseNoConstant(self):
+        print(f"\t Validate: (lambda x: x) -> []")
+
+        s_lambda = SmartLambda(lambda x: x)
+        constants = s_lambda.constants
+
+        self.assertEqual([], constants, f"Smart-Lambda parsed constants not matching: {[]} != {constants}")
+
+    @test("SMART-LAMBDA PARSE NONE-CONSTANT")
+    def testParseNoneConstant(self):
+        print(f"\t Validate: (lambda: None) -> [None]")
+
+        s_lambda = SmartLambda(lambda: None)
+        constants = s_lambda.constants
+
+        self.assertEqual([None], constants, f"Smart-Lambda parsed constants not matching: {[None]} != {constants}")
+
+    @test("SMART-LAMBDA PARSE INT-CONSTANT")
+    def testParseIntConstant(self):
         print(f"\t Validate: (lambda: 1) -> [1]")
 
         s_lambda = SmartLambda(lambda: 1)
         constants = s_lambda.constants
 
         self.assertEqual([1], constants, f"Smart-Lambda parsed constants not matching: {[1]} != {constants}")
+
+    @test("SMART-LAMBDA PARSE STR-CONSTANT")
+    def testParseStrConstant(self):
+        print(f"\t Validate: (lambda: 'str') -> ['str']")
+
+        s_lambda = SmartLambda(lambda: 'str')
+        constants = s_lambda.constants
+
+        self.assertEqual(['str'], constants, f"Smart-Lambda parsed constants not matching: {['str']} != {constants}")
