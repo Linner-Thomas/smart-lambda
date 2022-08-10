9dbbf67a772358ee8fc74503ec3aa78c27bd28d0
Linner Thomas <linner.thomas@gmx.net>
2022-08-10 10:52:01 +0200

    Improved assert-error message
    Issue: #3

--- tests/test_smart_lambda.py
+++ tests/test_smart_lambda.py
@@ -8,17 +8,26 @@ class TestSmartLambda(unittest.TestCase):
     @test("SMART-LAMBDA CALL NO-PARAMETER", parameter= range(0, 5))
     def testNoParameter(self, val):
         print(f"\t Validate: (lambda: {val}) -> {val}")
+
         s_lambda = SmartLambda(lambda: val)
-        self.assertEqual(val, s_lambda(), "SmartLambda return value doesn't match!")
+        return_value = s_lambda()
+
+        self.assertEqual(val, return_value, f"Smart-Lambda return-value not matching: {val} != {return_value}")
 
     @test("SMART-LAMBDA CALL POSITIONAL-PARAMETER", parameter= range(0, 5))
     def testPositionalParameter(self, val):
         print(f"\t Validate: (lambda x: x) -> {val} with x = {val}")
+
         s_lambda = SmartLambda(lambda x: x)
-        self.assertEqual(val, s_lambda(val), "SmartLambda return value doesn't match!")
+        return_value = s_lambda(val)
+
+        self.assertEqual(val, return_value, f"Smart-Lambda return-value not matching: {val} != {return_value}")
 
     @test("SMART-LAMBDA CALL KEYWORD-PARAMETER", parameter= range(0, 5))
     def testKeywordParameter(self, val):
         print(f"\t Validate: (lambda x = 0: x) -> {val} with x = {val}")
+
         s_lambda = SmartLambda(lambda x = 0: x)
-        self.assertEqual(val, s_lambda(x = val), "")
+        return_value = s_lambda(x = val)
+
+        self.assertEqual(val, return_value, f"Smart-Lambda return-value not matching: {val} != {return_value}")
